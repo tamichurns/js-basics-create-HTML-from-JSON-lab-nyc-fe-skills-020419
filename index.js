0e2571c5cafba99795c4e9a323d0841610bcc87a
Tami Churns <tchurns@paperlesspost.com>
2019-03-27 23:13:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentloaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
+  
+  //1 find out which button they pressed
+  //2 get the data for that movie
+  //3 replace the template values with the real ones
+  
+  // find button they pressed
+  const titanicButton = document.getElementById('Titanic')
+  const terminatorButton = document.getElementById('Terminator 2')
+  
+  //add the event addEventListener
+  titanicButton.addEventListener('click', () => console.log('titaniccc'))
+  terminatorButtonButton.addEventListener('click', () => console.log('T2'))
+  
+  
 });
