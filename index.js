edf9575fab752c5d25d244feb8b0d81aac5cf9f6
Tami Churns <tchurns@paperlesspost.com>
2019-03-27 23:22:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentloaded", (e) => {
+document.addEventListener("DOMContentLoaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
   
